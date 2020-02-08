dc7a59806d2e74a2c6c077910f01c3d147fa54e5
baoqinli <572191499@qq.com>
2020-02-08 14:57:33 +0800

    旅游项目

---
+++ travel/src/page/home/compoents/nav.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- 底部导航  跳转页面 -->
+    <div class="nav">
+      <router-link to="/">
+        <div>
+          <img src="" alt="">
+          <p>棣栭〉</p>
+        </div>
+      </router-link>
+      <router-link to="/personal">
+        <div>
+          <img src="" alt="">
+          <p>鎴戠殑</p>
+        </div>
+      </router-link>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  name:"Naver",
+
+}
+</script>
+
+<style scoped>
+@import url("../../../assets/style/public.css");
+</style>
